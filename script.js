1811204b3caa8e80caf2c0946cc54942b44db130
Robin Altman <robinhaltman@gmail.com>
2023-10-01 09:48:22 -0400

    message functions

--- script.js
+++ script.js
@@ -19,12 +19,16 @@ dragElement(document.getElementById("movable"));
       isDragging = true;
 
       // Start the interval only when dragging starts
-      if (isDragging) {setInterval(displayRandomImage, 2000);}
+      if (isDragging) {
+        // const randomInterval = Math.floor(Math.random() * (4000 - 2000 + 1)) + 2000;
+        // setInterval(displayRandomImage, randomInterval);
+        setInterval(displayRandomImage, 500);
+      }
 
       document.onmousemove = elementDrag;
     }
 
-    function elementDrag(e) { //drag
+    function elementDrag(e) {
       e = e || window.event;
       e.preventDefault();
       // calculate the new cursor position:
@@ -33,7 +37,6 @@ dragElement(document.getElementById("movable"));
       pos3 = e.clientX;
       pos4 = e.clientY;
 
-      //populate environment
       console.log("dragging");
       
       // calculate the boundaries of the parent element
@@ -55,7 +58,7 @@ dragElement(document.getElementById("movable"));
       elmnt.style.left = newX + "px";
     }
 
-    function closeDragElement() { //no drag
+    function closeDragElement() {
       document.onmouseup = null;
       document.onmousemove = null;
       isDragging = false;
@@ -68,6 +71,11 @@ dragElement(document.getElementById("movable"));
     'assets/3.png',
   ];
 
+  var objects = [];
+  const textDocumentURL = 'assets/random-text.txt';
+  let val = 0;
+  let messageDisplaying = false;
+
   function displayRandomImage() {
     if (isDragging) {
       const imgContainer = document.getElementById('img-container');
@@ -78,12 +86,72 @@ dragElement(document.getElementById("movable"));
       const img = document.createElement('img');
       img.src = randomImageUrl;
       
-      // Set random position within the container
-      img.style.left = Math.floor(Math.random() * (imgContainer.offsetWidth - 50)) + 'px';
-      img.style.top = Math.floor(Math.random() * (imgContainer.offsetHeight - 50)) + 'px';
+      // Set random position within container
+      var imgx = Math.floor(Math.random() * (imgContainer.offsetWidth - 50)) + 'px';
+      var imgy = Math.floor(Math.random() * (imgContainer.offsetHeight - 50)) + 'px';
+      img.style.left = imgx;
+      img.style.top = imgy;
+
+      const objectIndex = objects.length;
+      objects.push(img);
+
+      console.log("array: " + objects.length);
+
+      img.addEventListener('click', async function(event) {
+        console.log("Image clicked! : " + objectIndex);
+
+        if(messageDisplaying == false){
+          var messageDiv = document.createElement('div');
+          messageDiv.style.position = 'absolute';
+          messageDiv.style.width = '150px';
+          messageDiv.style.height = 'auto';
+          messageDiv.style.border = '1px solid';
+          messageDiv.style.backgroundColor = "white";
+          var message = document.createElement('p');
+          const response = await fetch(textDocumentURL);
+          const text = await response.text();
+          const lines = text.split('\n');
+          const randomIndices = getRandomIndices(lines.length, 3);
+          console.log("LINE: " + lines[randomIndices[0]]);
+          val = Math.floor(Math.random() * 5) + 1;
+          message.textContent = lines[randomIndices[0]];
+          message.style.fontStyle = "italic";
+
+          messageDiv.appendChild(message);
+          this.parentElement.appendChild(messageDiv);
+          messageDisplaying = true;
+
+          // Get the position of the clicked image
+          const imgRect = this.getBoundingClientRect();
+
+          // Set the top and left positions for the messageDiv
+          messageDiv.style.top =  imgRect.top + 'px';
+          messageDiv.style.left = imgRect.left + 60 + 'px';
+          messageDiv.style.zIndex = "5";
+
+          setTimeout(function() {
+            messageDiv.remove();
+            messageDisplaying = false;
+          }, 5000);
+
+        }
+
+      });
 
       imgContainer.appendChild(img);
     }
   }
+
+  //RANDOM TEXT
+  function getRandomIndices(max, count) {
+      const indices = [];
+      while (indices.length < count) {
+        const randomIndex = Math.floor(Math.random() * max);
+        if (!indices.includes(randomIndex)) {
+          indices.push(randomIndex);
+        }
+      }
+      return indices;
+    }
 
 
